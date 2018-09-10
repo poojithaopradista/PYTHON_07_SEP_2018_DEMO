ee7fa4b3217c6c07d0bcf7bafc0828ee930f0f88
srikanthpragada <srikanthpragada@gmail.com>
2018-09-10 09:03:32 +0530

    10-SEP

---
+++ even.py
@@ -0,0 +1,7 @@
+# Even or odd
+
+num = int(input("Enter a number :"))
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
---
+++ print_numbers.py
@@ -0,0 +1,5 @@
+# Print 1 to 10
+
+for i in range(1,11,2):
+    print(i,end = ' ')
+
